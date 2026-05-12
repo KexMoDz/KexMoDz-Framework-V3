91a70ec3168549d3382539aba29822a6ab65f128
KexMoDz <danielknack12@gmail.com>
2026-05-12 15:27:20 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local gameId = game.PlaceId
-if gameId == 6765805766 then -- ID für Block Spin
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KexMoDz/KexMoDz-Framework-V3/main/BlockSpin.lua?cache=" .. tick()))()
+if gameId == 6765805766 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KexMoDz/KexMoDz-Framework-V3/main/KexHub.lua?t=" .. os.time()))()
 else
-    print("Spiel wird nicht unterstützt")
+    print("Game not supported")
 end
