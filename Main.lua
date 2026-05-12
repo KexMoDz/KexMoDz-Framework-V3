aa3d1a9e3de15d7bb948da57edbed93c3f3bb6e4
KexMoDz <danielknack12@gmail.com>
2026-05-12 15:46:04 +0200

    Delete Main.lua

--- Main.lua
+++
@@ -1,6 +0,0 @@
-local gameId = game.PlaceId
-if gameId == 6765805766 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KexMoDz/KexMoDz-Framework-V3/main/KexHub.lua?t=" .. os.time()))()
-else
-    print("Game not supported")
-end
